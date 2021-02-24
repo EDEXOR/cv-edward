e0a90be029b72916793ac26cd7b13ed5859c88f1
Edward <edexorsoft@gmail.com>
2021-02-23 19:06:19 -0600

    Terminado

--- gulpfile.js
+++ gulpfile.js
@@ -21,17 +21,13 @@ gulp.task('style',function () {
 
 //minify all js to one js file
 gulp.task('minscripts', function() {
-    return gulp.src(['assets/js/jquery-migrate.js', 'assets/js/jquery.waypoints.min.js','assets/js/popper.js', 'assets/js/bootstrap.min.js','assets/js/wow.min.js','assets/js/bootstrap-progressbar.min.js', 'assets/js/jquery.counterup.min.js','assets/js/slick.min.js', 'assets/js/jquery.magnific-popup.min.js' ,'assets/js/jquery.filterizr.min.js'])
+    return gulp.src(['assets/js/jquery-migrate.js', 'assets/js/jquer
+        y.waypoints.min.js','assets/js/popper.js', 'assets/js/bootstrap.min.js','assets/js/wow.min.js','assets/js/bootstrap-progressbar.min.js', 'assets/js/jquery.counterup.min.js','assets/js/slick.min.js', 'assets/js/jquery.magnific-popup.min.js' ,'assets/js/jquery.filterizr.min.js'])
         .pipe(concat('plugins.js'))
         .pipe(gulp.dest('assets/'));
 });
 
-//minify all css to one css file
-
-gulp.task('mincss',function () {
-    return gulp.src(['assets/css/bootstrap.min.css','assets/css/animate.min.css','assets/css/et-line.css','assets/css/font-awesome.min.css','assets/css/magnific-popup.css','assets/css/slick.css','assets/css/ionicons.min.css'])
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(concat('style.min.css'))
+//minify all css to one css file    
         .pipe(gulp.dest('assets/'))
 });
 
